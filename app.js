26d4126da645a4fc37ef623cb4515c158f4f837c
phlgr <philippm.gartz@gmail.com>
2020-03-23 18:01:59 +0100

    Outsource

--- app.js
+++ app.js
@@ -1,50 +1,13 @@
-const fs = require("fs");
-const { readPasswords, writePasswords } = require("./lib/passwords");
-
 const [command, key, value] = process.argv.slice(2);
 
-function get() {
-  console.log("Called GET", key);
-  // Read and log db.json
-  try {
-    const passwords = readPasswords();
-
-    console.log(key, passwords[key]);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-function set() {
-  console.log("Called SET", key, value);
-  try {
-    const passwords = readPasswords();
-    passwords[key] = value;
-    writePasswords(passwords);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function unset() {
-  console.log("Called SET", key, value);
-  const dbJSON = fs.readFileSync("db.json", "utf8");
-  const passwords = JSON.parse(dbJSON);
-  delete passwords[key];
-  console.log(passwords[key]);
-  try {
-    fs.writeFileSync("db.json", JSON.stringify(passwords, null, 2));
-  } catch (error) {
-    console.error(error);
-  }
-}
+const { get, set, unset } = require("./lib/commands");
 
 if (command === "get") {
-  get();
+  get(key);
 } else if (command === "set") {
-  set();
+  set(key, value);
 } else if (command === "unset") {
-  unset();
+  unset(key, value);
 } else {
   console.error("Unknown command");
 }

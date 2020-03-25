ba5f8409fc745e5fce536ff0651a1110d5597dc5
phlgr <philippm.gartz@gmail.com>
2020-03-25 11:12:54 +0100

    Add encryption features

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const [command, key, value] = process.argv.slice(2);
 
-const { get, set, unset } = require("./lib/commands");
+const { get, set, unset, reset, changeMasterPass } = require("./lib/commands");
+const { askForMasterPassword, askForPassword } = require("./lib/input");
+const { readMasterPass } = require("./lib/passwords");
 
-if (command === "get") {
-  get(key);
-} else if (command === "set") {
-  set(key, value);
-} else if (command === "unset") {
-  unset(key, value);
-} else {
-  console.error("Unknown command");
+async function run() {
+  const checkMasterPass = await askForMasterPassword(`Enter Master Password: `);
+  if (checkMasterPass !== readMasterPass()) {
+    console.log("Password incorrect. Try again!");
+    return;
+  }
+  if (command === "get") {
+    get(key);
+  } else if (command === "set") {
+    const password = await askForPassword(`Enter password of ${key}: `);
+    set(key, password);
+  } else if (command === "unset") {
+    unset(key, value);
+  } else if (command == "CHANGE") {
+    changeMasterPass();
+  } else {
+    console.error("Unknown command");
+  }
 }
+
+run();
